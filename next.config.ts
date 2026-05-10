fdc6f7b879d7f07cc0b30944d359251fbb0b5ea1
whjin <wuhuajin09@163.com>
2026-05-10 08:29:09 +0800

    build: 部署到 vercel 平台

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export',
-  basePath: '/next-ai-core',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
